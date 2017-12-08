a010e98ea340bb852e97df14a04db230ab9c6d84
Chris Saylor <cjsaylor@gmail.com>
2017-12-07 19:35:34 -0500

    Extracted some of the tag retrieval code.
    This will make the list-tags command easier and consolidated logic.

--- commands/index.go
+++ commands/index.go
@@ -20,6 +20,36 @@ type entryHeader struct {
 	Content string   `fm:"content" yaml:"-"`
 }
 
+func tagMap(journalPath string) (map[string][]string, error) {
+	directory := journalPath + "/entries"
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+		return nil, err
+	}
+	index := make(map[string][]string)
+	for _, file := range files {
+		content, err := ioutil.ReadFile(fmt.Sprintf("%s/%s", directory, file.Name()))
+		if err != nil {
+			return nil, err
+		}
+		head := new(entryHeader)
+		frontmatter.Unmarshal(content, head)
+		for _, tag := range head.Tags {
+			index[tag] = append(index[tag], strings.TrimSuffix(file.Name(), ".md"))
+		}
+	}
+	return index, nil
+}
+
+func sortedTagKeys(index map[string][]string) []string {
+	keys := make([]string, 0, len(index))
+	for key := range index {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
 // NewIndexCommand creates a new command runner for index command
 func NewIndexCommand(config Configuration) *IndexCommand {
 	indexCommand := IndexCommand{
@@ -30,30 +60,12 @@ func NewIndexCommand(config Configuration) *IndexCommand {
 
 // Run the index command
 func (i *IndexCommand) Run(ctx context.Context, subcommandArgs []string) {
-	directory := i.options.JournalPath + "/entries"
-	files, err := ioutil.ReadDir(directory)
+	index, err := tagMap(i.options.JournalPath)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	index := make(map[string][]string)
-	for _, file := range files {
-		content, err := ioutil.ReadFile(fmt.Sprintf("%s/%s", directory, file.Name()))
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(2)
-		}
-		head := new(entryHeader)
-		frontmatter.Unmarshal(content, head)
-		for _, tag := range head.Tags {
-			index[tag] = append(index[tag], strings.TrimSuffix(file.Name(), ".md"))
-		}
-	}
-	keys := make([]string, 0, len(index))
-	for key := range index {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys)
+	keys := sortedTagKeys(index)
 	var newIndex string
 	for _, tag := range keys {
 		newIndex += fmt.Sprintf("\n* *%s* ", tag)
@@ -69,6 +81,6 @@ func (i *IndexCommand) Run(ctx context.Context, subcommandArgs []string) {
 	err = ioutil.WriteFile(i.options.JournalPath+"/Index.md", []byte(newIndex), 0644)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(3)
+		os.Exit(2)
 	}
 }

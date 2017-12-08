d4c7c8a6472a62761bb8c55a8a03dc0d9e7f5c03
Chris Saylor <cjsaylor@gmail.com>
2017-12-07 20:26:40 -0500

    Allow customizing the output path of the indexer command.
    Closes #1.

--- commands/index.go
+++ commands/index.go
@@ -2,9 +2,11 @@ package commands
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
 	"sort"
 	"strings"
 
@@ -13,6 +15,7 @@ import (
 
 type IndexCommand struct {
 	options Configuration
+	flags   *flag.FlagSet
 }
 
 type entryHeader struct {
@@ -54,16 +57,24 @@ func sortedTagKeys(index map[string][]string) []string {
 func NewIndexCommand(config Configuration) *IndexCommand {
 	indexCommand := IndexCommand{
 		options: config,
+		flags:   flag.NewFlagSet("index", flag.ExitOnError),
 	}
 	return &indexCommand
 }
 
 // Run the index command
 func (i *IndexCommand) Run(ctx context.Context, subcommandArgs []string) {
+	outputPath := i.flags.String("o", "Index.md", "Output path contained to the $JOURNAL_PATH.")
+	if !i.flags.Parsed() {
+		i.flags.Parse(subcommandArgs)
+	}
+	if *outputPath == "." {
+		*outputPath = "Index.md"
+	}
 	index, err := tagMap(i.options.JournalPath)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		os.Exit(2)
 	}
 	keys := sortedTagKeys(index)
 	var newIndex string
@@ -78,9 +89,10 @@ func (i *IndexCommand) Run(ctx context.Context, subcommandArgs []string) {
 		}
 		newIndex += strings.Join(mappedEntries, ", ")
 	}
-	err = ioutil.WriteFile(i.options.JournalPath+"/Index.md", []byte(newIndex), 0644)
+	indexPath := fmt.Sprintf("%s/%s", i.options.JournalPath, path.Base(*outputPath))
+	err = ioutil.WriteFile(indexPath, []byte(newIndex), 0644)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(2)
+		os.Exit(3)
 	}
 }
